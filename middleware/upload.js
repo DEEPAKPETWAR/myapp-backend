f1e644698ce13da4d3c60ddb39b40a468e0ecdc6
DEEPAK SHARMA <dpetwar@gmail.com>
2026-05-18 10:44:26 +0530

    fixed update profile

--- middleware/upload.js
+++ middleware/upload.js
@@ -2,15 +2,13 @@ const multer = require("multer");
 const path = require("path");
 
 const storage = multer.diskStorage({
-
-  destination: function (req, file, cb) {
+  destination: (req, file, cb) => {
     cb(null, "uploads/");
   },
-
-  filename: function (req, file, cb) {
-
+  filename: (req, file, cb) => {
     const uniqueName =
-      Date.now() + path.extname(file.originalname);
+      Date.now() + "-" + Math.round(Math.random() * 1e9) +
+      path.extname(file.originalname);
 
     cb(null, uniqueName);
   },
