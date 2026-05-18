dcd904661a88f2b937c7a29ebc017b8b83f5128e
DEEPAK SHARMA <dpetwar@gmail.com>
2026-05-18 12:03:49 +0530

    fixed update profile

--- middleware/upload.js
+++ middleware/upload.js
@@ -5,15 +5,29 @@ const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "uploads/");
   },
+
   filename: (req, file, cb) => {
     const uniqueName =
-      Date.now() + "-" + Math.round(Math.random() * 1e9) +
-      path.extname(file.originalname);
+      Date.now() + "-" + Math.round(Math.random() * 1e9);
 
-    cb(null, uniqueName);
+    cb(
+      null,
+      uniqueName + path.extname(file.originalname)
+    );
   },
 });
 
-const upload = multer({ storage });
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image/")) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only images allowed"), false);
+  }
+};
+
+const upload = multer({
+  storage,
+  fileFilter,
+});
 
 module.exports = upload;
