a2658a7c448d81f3c5bb7fd802d76d87521cd5dc
DEEPAK SHARMA <dpetwar@gmail.com>
2026-05-19 17:17:07 +0530

    fixed update profile

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -15,7 +15,7 @@ const authMiddleware = (req, res, next) => {
     const token = authHeader.split(" ")[1];
 
     // 3. Verify token
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+   const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
 req.userId = decoded.id;
 
